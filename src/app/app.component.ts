ef282b93706800d7641f3acc98f3e678394f476f
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-03-12 22:01:22 +0800

    login form. user session info stroage update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Backend, User, Test } from './angular-backend/angular-backend.module';
+import { Backend, User, Test,
+  USER, USER_LOGIN, USER_LOGIN_RESPONSE, USER_LIST_RESPONSE,
+  USER_LOGOUT_RESPONSE
+} from './angular-backend/angular-backend.module';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,24 +10,51 @@ import { Backend, User, Test } from './angular-backend/angular-backend.module';
 })
 export class AppComponent {
   title = 'app works!';
+
+
+  _id: string = null;
+  _password: string = null;
+
+  newUsers: Array<USER> = null;
+
   constructor(
-    // test: Test,
+//    test: Test,
     private backend: Backend,
     private user: User
   )
   {
-
-
+    // this.onClickLogin( 'admin', 'admin' );
     this.loadNewlyRegisteredUsers();
-
   }
 
   loadNewlyRegisteredUsers() {
 
-    this.user.list( {} ).subscribe( res => {
+    this.user.list( { order: 'idx DESC' } ).subscribe( (res: USER_LIST_RESPONSE) => {
+      this.newUsers = res.data.users;
       console.info(res);
     }, err => {
+      alert( err );
       console.error(err);
+    });
+  }
+
+  onClickLogin( id, password ) {
+    let req: USER_LOGIN = {
+      id: id,
+      password: password
+    };
+    this.user.login( req ).subscribe( (res: USER_LOGIN_RESPONSE ) => {
+      console.log( res );
+    }, err => {
+      this.user.alert( err );
+      console.log(err);
+    });
+  }
+  onClickLogout() {
+    this.user.logout().subscribe( (res: USER_LOGOUT_RESPONSE) => {
+      console.log( res );
+    }, err => {
+      this.user.alert( err );
     });
   }
 }
