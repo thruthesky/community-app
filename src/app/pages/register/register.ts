79a3fdb93465d344f327c8c5fd3025892a6b395c
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-03-22 00:20:55 +0800

    updating sample codes

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Backend, User, Test, File, PostConfig, PostData,
+
+import { User, Test, File,
+  USER,
   USER_REGISTER, USER_REGISTER_RESPONSE,
   USER_DATA_RESPONSE,
   SESSION_INFO,
@@ -8,23 +10,22 @@ import { Backend, User, Test, File, PostConfig, PostData,
   USER_EDIT,
   FILE_UPLOAD
 } from './../../angular-backend/angular-backend';
-import {USER} from "../../angular-backend/interface";
+
 @Component({
   selector: 'register-page',
   templateUrl: './register.html',
   styleUrls:['./register.css']
 })
 export class RegisterPage implements OnInit{
-  form = <USER_REGISTER>{};
-  user_data;
-  user_photo_idx: number = 0;
+  form: USER_REGISTER | USER_EDIT = {};
+  userData: USER_FIELDS = null;
+
+
   src_photo: string = null;
-  edit_src_photo: string = null;
+  // edit_src_photo: string = null;
 
   constructor(
-    private backend: Backend,
     private user: User,
-    private post: PostData,
     private file: File ) {
 
   }
@@ -34,24 +35,26 @@ export class RegisterPage implements OnInit{
 
 
     onClickRegister(){
-      // this.form.file_hooks = [ this.user_photo_idx ];
+      this.form.file_hooks = [ this.user_photo_idx ];
       this.user.register(this.form).subscribe((res: USER_REGISTER_RESPONSE) => {
         console.info(res);
       }, err => {
         this.user.alert(err);
       });
     }
+
   loadData(){
-    this.form = <USER_EDIT>{};
+    this.form = {};
     this.user.data().subscribe( ( res: USER_DATA_RESPONSE ) => {
-      this.form = res.data.user;
-      this.user_data = res.data.user;
-      this.edit_src_photo = this.file.src( { idx: this.user_data.primary_photo_idx });
+      this.userData = res.data.user;
+      this.form = this.userData;
+      console.log(this.userData);
+      this.src_photo = this.file.src( { idx: this.userData.primary_photo_idx });
       console.log('loaduserdata::res', res);
     }, err => this.user.alert(err));
   }
   onClickUpdate(){
-    let editdata = <USER_EDIT>{
+    let editdata:USER_EDIT = {
       email     : this.form.email,
       name      : this.form.name,
       nickname  : this.form.nickname
@@ -73,7 +76,7 @@ export class RegisterPage implements OnInit{
 
     this.file.upload(req, file).subscribe(res => {
       console.log(res);
-      this.user_photo_idx = res.data.idx;
+      
       this.src_photo = this.file.src( { idx: res.data.idx } );
     }, err => {
       console.log('error', err);
@@ -86,7 +89,7 @@ export class RegisterPage implements OnInit{
     console.log("file: ", file);
     let req: FILE_UPLOAD = {
       model: 'user',
-      model_idx: this.user_data.idx,
+      model_idx: this.userData.idx,
       code: 'primary_photo',
       unique: 'Y',
       finish: 'Y'
@@ -94,8 +97,8 @@ export class RegisterPage implements OnInit{
 
     this.file.upload(req, file).subscribe(res => {
       console.log(res);
-      this.user_photo_idx = res.data.idx;
-      this.edit_src_photo = this.file.src( { idx: res.data.idx } );
+      
+      this.src_photo = this.file.src( { idx: res.data.idx } );
     }, err => {
       console.log('error', err);
     });
